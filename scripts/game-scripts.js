27f1678697b0cf9f00a14793cad6b2db67e4e24e
alex.linux <183350467+alx-unix@users.noreply.github.com>
2024-11-21 16:30:09 +0100

    +generateRandomWord,+footer,+userInput,+button fixed

--- scripts/game-scripts.js
+++ scripts/game-scripts.js
@@ -1,11 +1,25 @@
 function timer(){
     let sec = 60;
     let timer = setInterval(function(){
-        document.getElementById('timer').innerHTML='00:'+sec;
+        document.getElementById('timer').innerText='00:'+sec;
         sec--;
         if (sec < 0) {
             clearInterval(timer);
-            document.getElementById('timer').innerHTML="Fin"
+            document.getElementById('timer').innerText="Fin"
         }
     }, 1000);
 }
+
+
+function showRandomWord(difficulty,timer){
+    while (timer){
+        if (difficulty === "easy"){
+            generate({minLength:3,maxLength:5})
+        }else if (difficulty ==="medium"){
+            generate({minLength:5,maxLength:7})
+        }else {
+            generate({minLength:8})
+        }
+    }
+}
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,31 @@
 let startButton = document.getElementById("strButton");
+let submitDiffButton = document.getElementById("sbtButton");
+let diffChosen = document.getElementById("set-diff")
+let fieldsetDiff = document.getElementById("choose-diff-fieldset")
+let radioButtons = document.querySelectorAll('input[name="difficulty"]');
+let divWordShown = document.getElementById("wordChosen");
+
+
+
+
+
+
+
 startButton.addEventListener('click',()=>{
     timer()
 });
 
-let submitDiffButton = document.getElementById("sbtButton");
-let diffChosen = document.getElementById("set-diff")
+
+submitDiffButton.addEventListener("click",($event)=>{
+    $event.preventDefault();
+    let selected_diff;
+    for (let i of radioButtons){
+        if (i.checked){
+            selected_diff = i.value;
+            break;
+        }
+    }
+    fieldsetDiff.innerHTML = `You have chosen ${selected_diff} difficulty`
+})
+
+
